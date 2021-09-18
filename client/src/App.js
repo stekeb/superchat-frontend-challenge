7355d61b7c4fbfe2a3e53d11f50ea129b78975bd
Steffen Keilbach <s.keilbach@gmx.net>
2021-09-18 18:58:40 +0200

    feat: react router

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Card from './Card';
+import Cardcreator from './Cardcreator';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+function App() {
+  return (
+    <Router>
+    <div className="App">
+    <Switch>
+      <Route exact path="/">
+        <Cardcreator />
+      </Route>
+      <Route path="/card/:nanoId">
+        <Card />
+      </Route>
+    </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/Cardcreator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cardcreator(props) {
+  return (
+    <div>
+      <h1>CARDCREATOR</h1>
+    </div>
+  );
+}
+
+export default Cardcreator;
